40348a5d61e38e3d62d3ddc527103200eb2eede5
themarsfactor <olayemey@gmail.com>
2021-04-10 04:30:25 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*(function()
+{
+	alert ('working perfectly')
+})();//iife (immediately invoked function expression)
+
+let name = function(){
+	alert('working good');
+}
+name();// another way of invoking function
+
+function bee(){
+	alert('working brilliant');
+}
+bee();
